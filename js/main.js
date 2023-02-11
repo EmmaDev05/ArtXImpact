fd3fb44ff81d5f76880ffcf1df3c5e96b0917180
Emmanuele Ballarani <86883649+EmmaDev05@users.noreply.github.com>
2023-02-11 10:00:24 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,6 +12,46 @@ $(".menu-list li a").on("click", function () {
   $("#hamburger-menu").click();
 });
 
+// Countdown
+const countdown = document.querySelector(".countdown");
+
+//Set launch date
+const launchDate = new Date("Apr 06, 2023 17:00:00").getTime();
+
+//Update every second
+const invtl = setInterval(() => {
+  // Get todays date and time (ms)
+  const now = new Date().getTime();
+
+  //Distance from now to the launch date
+  const distance = launchDate - now;
+
+  //Time calculations
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display result
+  countdown.innerHTML = `
+   <div>${days}<span style="color:white">Days</span></div>
+   <div>${hours}<span style="color: white" >Hours</span></div>
+   <div>${mins}<span style="color: white">Minutes</span></div>
+   <div>${seconds}<span style="color: white">Seconds</span></div>
+   `;
+
+  //If launch date passed
+  if (distance < 0) {
+    //Stop countdown
+    clearInterval(intvl);
+    //Style and input text
+    countdown.style.color = "#17a2b8";
+    countdown.innerHTML = "Launched!";
+  }
+}, 1000);
+
 /* When your mouse cursor enter the background, the fading won't pause and keep playing */
 $(".carousel").carousel({
   pause:
@@ -72,43 +112,3 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
     }
   });
 });
-
-// Countdown
-const countdown = document.querySelector(".countdown");
-
-//Set launch date
-const launchDate = new Date("Apr 06, 2023 17:00:00").getTime();
-
-//Update every second
-const invtl = setInterval(() => {
-  // Get todays date and time (ms)
-  const now = new Date().getTime();
-
-  //Distance from now to the launch date
-  const distance = launchDate - now;
-
-  //Time calculations
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display result
-  countdown.innerHTML = `
-  <div>${days}<span style="color:white">Days</span></div>
-  <div>${hours}<span style="color: white" >Hours</span></div>
-  <div>${mins}<span style="color: white">Minutes</span></div>
-  <div>${seconds}<span style="color: white">Seconds</span></div>
-  `;
-
-  //If launch date passed
-  if (distance < 0) {
-    //Stop countdown
-    clearInterval(intvl);
-    //Style and input text
-    countdown.style.color = "#17a2b8";
-    countdown.innerHTML = "Launched!";
-  }
-}, 1000);
